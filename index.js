1a9feadff3a1d439d3a4bff2e470efa27eef8de8
thopatevijay <thopatevijay@gmail.com>
2022-09-17 20:23:55 +0530

    feat : init funcs and front end

---
+++ index.js
@@ -0,0 +1,12 @@
+const billedAmount = document.querySelector("#bill-amount");
+const givenAmount = document.querySelector("#cash-given");
+const checkButton = document.querySelector(".check-button");
+
+
+
+function handleFeeCalculations() {
+    console.log('checking')
+}
+
+
+checkButton.addEventListener("click", handleFeeCalculations)
